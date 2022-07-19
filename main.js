2e40caec39531acdad0135047b4565c0653e401b
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-07-19 03:54:58 -0400

    CompletedAssignment

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modalMessage = document.querySelector('.hidden')
 
-// Your JavaScript code goes here!
+//iterate over each button and pass it through to the event listener for the eventDisposition function
 
+const array = document.querySelectorAll('.like-glyph')
 
+array.forEach((ele) => {
+ ele.closest('article').addEventListener('click', (event) => { 
+  event.composedPath()[3].addEventListener('click', eventDisposition(event.composedPath()[3]))})
 
+function eventDisposition(block) {
+  return (block.querySelector('span').className === 'activated-heart'?handleFullHeart(block):handleClick(block))
+
+  function handleClick(block){
+
+  function errorHandler(){
+  modalMessage.className = 'none'
+  modalMessage.querySelector('h2').textContent = error
+  setTimeout(clearMessage, 3000)
+  }
+  function clearMessage () {
+    modalMessage.className = 'hidden'
+  }
+
+mimicServerCall()
+.then(res => {
+  block.querySelector('span').textContent = FULL_HEART
+  block.querySelector('span').className = 'activated-heart'
+  })
+.catch(err => {
+  error = err
+  errorHandler()
+})
+}
+
+function handleFullHeart(block){
+  block.querySelector('span').textContent = EMPTY_HEART
+  block.querySelector('span').className = 'like-glyph'
+  }}
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
